0321deebb9aa5dbd626abaafbbd182e286dec9df
Kadagazov <antonkadagazov@gmail.com>
2018-02-25 21:30:21 +0200

    automatic apload

--- pub.cmd
+++ pub.cmd
@@ -3,3 +3,5 @@ git add .
 git commit -m "first commit"
 git remote add origin %1
 git push origin master
+git branch gh-pages
+git checkout gh-pages && git merge master && git checkout master && git push --all
---
+++ upd.cmd
@@ -0,0 +1,2 @@
+git add . && git commit -m "automatic apload"
+git checkout gh-pages && git merge master && git checkout master && git push --all
